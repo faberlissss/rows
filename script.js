b6492db1a00003a98d83d0a1b81cb3ecc38ce981
faberlissss <maria.simochka@gmail.com>
2023-12-22 17:31:19 +0200

    new commit

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let age = prompt('How old are you?');
+let yourname = prompt('What is your name?');
+let surname = prompt('What is your surname?');
+alert(`${yourname} ${age} ${surname}`);
